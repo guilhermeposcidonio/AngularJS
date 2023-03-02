4fd79dccbbf3cb2d7d361aacae5ed284995082cd
Poscidonio <guilhermeposcidonio@gmail.com>
2023-03-02 17:29:09 -0300

    Diretivas

--- primeiro-projeto/src/app/app.component.ts
+++ primeiro-projeto/src/app/app.component.ts
@@ -1,45 +1,15 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-  <app-title *ngIf='destruir'></app-title>
-  <button (click)='destruirComponent()'>Destruir Componente</button>
+  <app-data-biding></app-data-biding>
   <router-outlet></router-outlet>`
 })
-export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
-  public destruir: boolean = true;
-  constructor() { }
+export class AppComponent implements OnInit {
 
-  public destruirComponent() {
-    this.destruir = false;
-  }
+  constructor() { }
   //void é quando se espera um retorno 
   ngOnInit(): void {
-    //sera impressso no console apos os 5 s
-    setTimeout(() => {
-      console.log(1);
-    }, 5000)
-  }
-
-  ngDoCheck(): void {
-    console.log('ngDoCheck');
-
-  }
-  ngAfterContentInit(): void {
-    console.log('ngAfterContentInit');
-
-  }
-  ngAfterContentChecked(): void {
-    console.log('ngAfterContentChecked');
-
-  }
-  ngAfterViewInit(): void {
-    console.log('ngAfterViewInit');
-
-  }
-  ngAfterViewChecked(): void {
-    console.log('ngAfterViewChecked');
-
   }
 }
