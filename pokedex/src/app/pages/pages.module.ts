53a1d586d1c72eb38738f8b199fda8203a381c5b
Poscidonio <guilhermeposcidonio@gmail.com>
2023-03-28 23:26:05 -0300

    Iniciando os componentes da pagina.

--- pokedex/src/app/pages/pages.module.ts
+++ pokedex/src/app/pages/pages.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 import { DetailsComponent } from './details/details.component';
 import { HomeComponent } from './home/home.component';
 import { RoutingModule } from './routing.module';
@@ -13,7 +14,8 @@ import { RoutingModule } from './routing.module';
   ],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    SharedModule
   ]
 })
 export class PagesModule { }
